e7edbad30a76791f5e255542a11fc5a0c21a417e
Lugman <lugman.s@grootan.com>
2022-10-14 12:23:06 +0530

    Client added

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import router from "./router/router.js";
 import http from "http";
 import https from "https";
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const key = fs.readFileSync("./certs/ssl.key", "utf-8");
 const cert = fs.readFileSync("./certs/ssl.crt", "utf-8");
@@ -19,6 +21,19 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "./client")));
+
+app.get("/dashboard", (req, res) => res.sendFile(path.join(__dirname, "./client", "index.html")));
+
+app.get("/dashboard/tasks", (req, res) => res.sendFile(path.join(__dirname, "./client", "index.html")));
+
+app.get("/dashboard/assign", (req, res) => res.sendFile(path.join(__dirname, "./client", "index.html")));
+
 app.use(router);
 
 const HTTP_PORT = 3000;
